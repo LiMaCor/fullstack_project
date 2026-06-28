47a839123a0371aa9e807c01e99eb287983b0442
LiMaCor <julian.itsa8@gmail.com>
2026-06-28 18:56:14 +0200

    Added "Content" component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,28 +5,38 @@ const Header = (props) => {
     )
 }
 
+const Content = (props) => {
+  console.log("Content component added.")
+  return (
+    <>
+      <p>
+      {props.exercises[0].part} {props.exercises[0].exercises}
+      </p>
+      <p>
+        {props.exercises[1].part} {props.exercises[1].exercises}
+      </p>
+      <p>
+        {props.exercises[2].part} {props.exercises[2].exercises}
+      </p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const exercises = [
+    {part: "Fundamentals of React", exercises: 10}, 
+    {part: "Using props to pass data", exercises: 7}, 
+    {part: "State of a component", exercises: 14}
+  ]
+
+  const listExercises = [10, 14, 7];
 
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content exercises={exercises} />
     </div>
   )
 }
